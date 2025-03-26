680d869b98d648758df01491dfd58fb049285daf
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-03-26 14:38:20 +0500

    Finisheddd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,61 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorModule } from './author/author.module';
 import { ProfileModule } from './profile/profile.module';
 import { BookModule } from './book/book.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './entities/author-entity';
 import { Book } from './entities/book-entity';
 import { Profile } from './entities/profile-entity';
 
 @Module({
-  imports: [AuthorModule, ProfileModule, BookModule, 
+  imports: [
+    AuthorModule,
+    ProfileModule,
+    BookModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres', 
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'yusuf777$',
+      port: 5432, 
+      username: 'postgres', 
+      password: '12345678', 
       database: 'homework_typeorm',
-      extra: {
-        authPlugin: 'caching_sha2_password', 
-      },
       entities: [Author, Book, Profile],
-      // synchronize: true,
+      synchronize: true, 
     }),
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+// // import { AppController } from './app.controller';
+// // import { AppService } from './app.service';
+// import { AuthorModule } from './author/author.module';
+// import { ProfileModule } from './profile/profile.module';
+// import { BookModule } from './book/book.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Author } from './entities/author-entity';
+// import { Book } from './entities/book-entity';
+// import { Profile } from './entities/profile-entity';
+
+// @Module({
+//   imports: [AuthorModule, ProfileModule, BookModule, 
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: 'yusuf777$',
+//       database: 'homework_typeorm',
+//       extra: {
+//         authPlugin: 'caching_sha2_password', 
+//       },
+//       entities: [Author, Book, Profile],
+//       // synchronize: true,
+//     }),
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+// export class AppModule {}
